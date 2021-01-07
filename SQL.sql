75c8b834148a89637f322aead38848328402ec77
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-01-07 09:34:17 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,3 +1,4 @@
 SELECT * FROM departements JOIN regions ON departements.num_region = regions.num_region WHERE regions.nom = "Bretagne" OR departements.nom = "Pays de la Loire"
 SELECT count(*) as "Nombre de communes", departements.nom FROM communes JOIN departements ON departements.num_departement = communes.num_departement GROUP BY communes.num_departement ORDER BY population_2010 DESC
 SELECT regions.nom, AVG(communes.population_2010/communes.surface) as "densité" FROM regions JOIN communes JOIN departements ON regions.num_region = departements.num_region AND departements.num_departement = communes.num_departement GROUP BY regions.nom 
+SELECT DISTINCT departements.nom FROM departements JOIN communes ON departements.num_departement = communes.num_departement WHERE communes.nom LIKE "Petit%"
