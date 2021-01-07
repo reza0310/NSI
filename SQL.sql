7e75e51cfd5a28d7ee33f3d21c63c1ec1043cbf5
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-01-07 09:16:38 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,1 +1,2 @@
 SELECT * FROM departements JOIN regions ON departements.num_region = regions.num_region WHERE regions.nom = "Bretagne" OR departements.nom = "Pays de la Loire"
+SELECT count(*) as "Nombre de communes", departements.nom FROM communes JOIN departements ON departements.num_departement = communes.num_departement GROUP BY communes.num_departement ORDER BY population_2010 DESC
