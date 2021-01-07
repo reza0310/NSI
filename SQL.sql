300b65e26d391872e4e028e4212d77667d4e4b23
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-01-07 09:06:21 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,1 +1,1 @@
-SELECT * FROM departements JOIN regions WHERE 
+SELECT * FROM departements JOIN regions ON departements.num_region = regions.num_region WHERE regions.nom = "Bretagne" OR departements.nom = "Pays de la Loire"
