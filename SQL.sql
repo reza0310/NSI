422419d7fdc1aa0ebdb41c316cc75e45c5ba8ef3
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-01-07 10:11:26 +0100

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,6 +1,6 @@
 SELECT * FROM departements JOIN regions ON departements.num_region = regions.num_region WHERE regions.nom = "Bretagne" OR departements.nom = "Pays de la Loire"
 SELECT count(*) as "Nombre de communes", departements.nom, sum(population_2010) FROM communes JOIN departements ON departements.num_departement = communes.num_departement GROUP BY communes.num_departement ORDER BY sum(population_2010) DESC 
-SELECT regions.nom, AVG(communes.population_2010/communes.surface) as "densité" FROM regions JOIN communes JOIN departements ON regions.num_region = departements.num_region AND departements.num_departement = communes.num_departement GROUP BY regions.nom 
+SELECT regions.nom, AVG(communes.population_2010/communes.surface) as "densité" FROM regions JOIN communes JOIN departements ON regions.num_region = departements.num_region AND departements.num_departement = communes.num_departement GROUP BY regions.nom   //remplacer AVG(communes.population_2010/communes.surface) par sum(communes.population_2010)/sum(communes.surface)
 SELECT DISTINCT departements.nom FROM departements JOIN communes ON departements.num_departement = communes.num_departement WHERE communes.nom LIKE "Petit%"
 
 SELECT * FROM communes WHERE population_2010 > 100*(SELECT avg(population_2010) FROM communes)
