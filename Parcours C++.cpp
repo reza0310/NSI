3cceefd7edc8a8348f6e5cee404b4604d861b951
reza0310 <70545529+reza0310@users.noreply.github.com>
2021-01-11 22:08:50 +0100

    Terminé aussi!
    Et dans les temps!

--- Parcours C++.cpp
+++ Parcours C++.cpp
@@ -1,54 +1,56 @@
 #include <iostream>
 
-char f[2] = {NULL, NULL};
-char h[2] = {NULL, NULL};
-char i[2] = {NULL, NULL};
-char k[2] = {NULL, NULL};
-char m[2] = {NULL, NULL};
-char j[2] = {*m, NULL};
-char e[2] = {*k, NULL};
-char d[2] = {*i, *j};
-char c[2] = {NULL, *h};
-char b[2] = {*e, *f};
-char a[2] = {*c, *d};
-char r[2] = {*a, *b};
 
-void parcoursPrefixe(char* arbre) {
-    std::cout << arbre << ", " << std::endl;
-    if (arbre[0] != NULL) {
-        parcoursPrefixe(&arbre[0]);
-	}
-    if (arbre[1] != NULL) {
-        parcoursPrefixe(&arbre[1]);
-	}
+char arbre[6][18] = { {'r', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+                   {'a', 'b', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+                   {'c', 'd', 'e', 'f', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+                   {NULL, 'h', 'i', 'j', 'k', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+                   {NULL, NULL, NULL, NULL, NULL, NULL, 'm', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+                   {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL} };
+
+char* get_etage(int x) {
+    return arbre[x];
 }
 
-void parcoursInfixe(char* arbre) {
-    if (arbre[0] != NULL) {
-        parcoursInfixe(&arbre[0]);
-	}
-    std::cout << arbre << ", " << std::endl;
-    if (arbre[1] != NULL) {
-        parcoursInfixe(&arbre[1]);
-	}
+void parcoursPrefixe(char* ligne, int etage, int place) {
+    std::cout << ligne[place] << ", ";
+    if (get_etage(etage + 1)[2*place] != NULL) {
+        parcoursPrefixe(get_etage(etage + 1), etage + 1, 2 * place);
+    }
+    if (get_etage(etage + 1)[2*place + 1] != NULL) {
+        parcoursPrefixe(get_etage(etage + 1), etage + 1, 2 * place + 1);
+    }
 }
 
-void parcoursPostfixe(char* arbre) {
-    if (arbre[0] != NULL) {
-        parcoursPostfixe(&arbre[0]);
-	}
-    if (arbre[1] != NULL) {
-        parcoursPostfixe(&arbre[1]);
-	}
-    std::cout << arbre << ", " << std::endl;
+void parcoursInfixe(char* ligne, int etage, int place) {
+    if (get_etage(etage + 1)[2*place] != NULL) {
+        parcoursInfixe(get_etage(etage + 1), etage + 1, 2 * place);
+    }
+    std::cout << ligne[place] << ", ";
+    if (get_etage(etage + 1)[2*place + 1] != NULL) {
+        parcoursInfixe(get_etage(etage + 1), etage + 1, 2 * place + 1);
+    }
+}
+
+void parcoursPostfixe(char* ligne, int etage, int place) {
+    if (get_etage(etage + 1)[2*place] != NULL) {
+        parcoursPostfixe(get_etage(etage + 1), etage + 1, 2 * place);
+    }
+    if (get_etage(etage + 1)[2*place + 1] != NULL) {
+        parcoursPostfixe(get_etage(etage + 1), etage + 1, 2 * place + 1);
+    }
+    std::cout << ligne[place] << ", ";
 }
 
 int main() {
-	parcoursPrefixe(r);
-	parcoursPostfixe(r);
-	parcoursInfixe(r);
-	
-	char fin;
-	std::cin >> fin;
-	return(0);
+    parcoursPrefixe(get_etage(0), 0, 0);
+    std::cout << std::endl;
+    parcoursPostfixe(get_etage(0), 0, 0);
+    std::cout << std::endl;
+    parcoursInfixe(get_etage(0), 0, 0);
+    std::cout << std::endl;
+
+    char fin;
+    std::cin >> fin;
+    return(0);
 }
